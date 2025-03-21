be818430eca9fbce5cca8d3822a3bc6d37314f41
igorthebarros <1igorhbarros@gmail.com>
2025-03-21 09:39:42 -0300

    :sparkles: Add Sale Controller

--- src/Ambev.DeveloperEvaluation.WebApi/Controllers/SaleController.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Controllers/SaleController.cs
@@ -1,23 +1,61 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿using Ambev.DeveloperEvaluation.Application.Commands.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.Application.Commands.Sales.DeleteSale;
+using Ambev.DeveloperEvaluation.Application.Commands.Sales.GetSale;
 using Ambev.DeveloperEvaluation.WebApi.Common;
-using Microsoft.AspNetCore.Http;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ambev.DeveloperEvaluation.WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/sale")]
     [ApiController]
     public class SaleController : BaseController
     {
-        public SaleController()
+        private readonly IMediator _mediator;
+
+        public SaleController(IMediator mediator)
         {
-                
+            _mediator = mediator;
         }
 
+        /// <summary>
+        /// Create a new sale
+        /// </summary>
         [HttpPost]
-        public IActionResult CreateSale(Sale sale)
+        public async Task<IActionResult> CreateSale([FromBody] CreateSaleCommand command)
         {
-            return Ok(sale);
+            // Improve method adding different resposnse types 
+            var result = await _mediator.Send(command).ConfigureAwait(false);
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get a sale by Id
+        /// </summary>
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById([FromHeader] GetSaleCommand command)
+        {
+            var result = await _mediator.Send(command).ConfigureAwait(false);
+
+            if (result == null)
+                return NotFound(new { message = "Sale not found" });
+
+            return Ok(result);
+        }
+        /// <summary>
+        /// Delete a sale
+        /// </summary>
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(DeleteSaleCommand command)
+        {
+            var result = await _mediator.Send(command).ConfigureAwait(false);
+
+            if (result == null)
+                return NotFound(new { message = "Sale not found" });
+
+            return NoContent();
+
         }
     }
 }
